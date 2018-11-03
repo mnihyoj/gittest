c6c4abd1271ead6795175611ed8eb3d60e778de0
iaihy@126.com <iaihy@126.com>
2018-11-03 10:28:04 +0800

    测试提交

--- time/src/test/java/hy/plugin/common/TimeToolTest.java
+++ time/src/test/java/hy/plugin/common/TimeToolTest.java
@@ -19,30 +19,32 @@ public class TimeToolTest {
     @Test
     public void getDay() {
         Date date = timeTool.getDay(System.currentTimeMillis());
-        logger.info("测试："+date.toString());
+        logger.info("getDay："+date.toString());
     }
 
     @Test
     public void getDay1() {
         Date date = timeTool.getDay("2012.01.12",ShowType.D_YEAR_DAY);
-        logger.info("测试："+date);
+        logger.info("getDay1："+date);
     }
 
     @Test
     public void getString() {
-        logger.info(timeTool.getString(timeTool.getDay("2012.01.12",ShowType.D_YEAR_DAY),ShowType.SYS_DF));
+        logger.info("getString："+timeTool.getString(timeTool.getDay("2012.01.12",ShowType.D_YEAR_DAY),ShowType.SYS_DF));
     }
 
     @Test
     public void getString1() {
-        logger.info(timeTool.getString(timeTool.getDay("2012.01.12",ShowType.D_YEAR_DAY),"YYYY-MM-DD"));
+        logger.info("getString1："+timeTool.getString(timeTool.getDay("2012.01.12",ShowType.D_YEAR_DAY),"YYYY-MM-DD"));
     }
 
     @Test
     public void showDay() {
+        logger.info("showDay："+timeTool.showDay(timeTool.getDay("2018.10.31",ShowType.D_YEAR_DAY)));
     }
 
     @Test
-    public void show() {
+    public void showTime() {
+        logger.info("showTime："+timeTool.showTime(timeTool.getDay("2018-11-3 10:12:00",ShowType.SYS_DF)));
     }
 }
